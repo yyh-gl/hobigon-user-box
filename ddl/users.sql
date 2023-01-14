99bcbe209f0661f76dcb53aa4f7cd42ae6bc673d
yyh-gl <yhonda.95.gl@gmail.com>
2023-01-15 02:09:55 +0900

    仮でDDL追加

---
+++ ddl/users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    `id`              CHAR(36) NOT NULL PRIMARY KEY,
+    `email`           VARCHAR(191) NOT NULL UNIQUE,
+    `hashed_password` VARCHAR(191) NOT NULL,
+    `salt`            VARCHAR(191) NOT NULL,
+    `user_name`       VARCHAR(191) NOT NULL UNIQUE,
+    `display_name`    VARCHAR(191) NOT NULL,
+    `created_at`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    INDEX idx_users_email (email),
+    INDEX idx_users_user_name (user_name)
+) DEFAULT CHARSET=utf8mb4;
